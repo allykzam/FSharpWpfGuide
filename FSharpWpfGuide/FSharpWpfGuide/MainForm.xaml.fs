95d1e2f8b9c21127686196f350abfbb0c83fc9a0
Anthony Perez (Work) <Anthony.Perez@hsn.net>
2014-12-17 14:55:54 -0500

    Added MainForm.xaml.fs

---
+++ FSharpWpfGuide/FSharpWpfGuide/MainForm.xaml.fs
@@ -0,0 +1,8 @@
+﻿namespace FSharpWpfGuide.Views
+
+type MainForm = FsXaml.XAML<"MainForm.xaml", true>
+
+namespace FSharpWpfGuide.ViewModels
+
+type MainFormViewModel() =
+    member __.Title = "F# |> WPF Guide"
