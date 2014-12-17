8b9b5674cfe691a0d500263aeb71c48f8c703fa3
Anthony Perez (Work) <Anthony.Perez@hsn.net>
2014-12-17 16:34:40 -0500

    Added Car model

---
+++ FSharpWpfGuide/FSharpWpfGuide/Models/Car.fs
@@ -0,0 +1,13 @@
+﻿namespace FSharpWpfGuide.Models
+
+type Make =
+    | Ford
+    | Chevy
+
+type Car =
+    {
+        Make : Make
+        Model : string
+        Year : int
+        Miles : int
+    }
