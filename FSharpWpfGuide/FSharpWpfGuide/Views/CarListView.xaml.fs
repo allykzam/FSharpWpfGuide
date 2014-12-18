95119140eab050e503ee55acbe641022ea8693a0
Anthony Perez (Work) <Anthony.Perez@hsn.net>
2014-12-18 10:13:30 -0500

    Added CarListViewController

--- FSharpWpfGuide/FSharpWpfGuide/Views/CarListView.xaml.fs
+++ FSharpWpfGuide/FSharpWpfGuide/Views/CarListView.xaml.fs
@@ -1,3 +1,17 @@
 ﻿namespace FSharpWpfGuide.Views
 
 type CarListView = FsXaml.XAML<"Views/CarListView.xaml", true>
+
+type CarListViewController() =
+    inherit FsXaml.UserControlViewController<CarListView>()
+
+    let buttonClick (parent : CarListView) _ =
+        let msg txt = System.Windows.MessageBox.Show(txt) |> ignore
+        match parent.CarList.SelectedItem with
+        | :? FSharpWpfGuide.ViewModels.CarViewModel as x ->
+            sprintf "Hello. You've selected the %s %s" x.MakeStr x.Model
+            |> msg
+        | _ -> msg "Hi. You haven't selected a car yet."
+
+    override self.OnInitialized view =
+        view.TestButton.Click.Subscribe (buttonClick view) |> self.DisposeOnUnload
