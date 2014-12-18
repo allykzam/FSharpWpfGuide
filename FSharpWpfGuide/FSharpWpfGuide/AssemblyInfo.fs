8eb8f2ddb85b777ab1903f36659d75faadc380d3
Anthony Perez (Work) <Anthony.Perez@hsn.net>
2014-12-18 10:35:40 -0500

    Added AssemblyInfo
    Although this isn't strictly needed in F#, it's helpful to include one

---
+++ FSharpWpfGuide/FSharpWpfGuide/AssemblyInfo.fs
@@ -0,0 +1,10 @@
+﻿namespace FSharpWpfGuide
+
+open System.Reflection
+
+module AssemblyInfo =
+    [<assembly: AssemblyTitle("Sample application developed to show how WPF and XAML can be utilized with F#")>]
+    [<assembly: AssemblyCopyright("Copyright © 2014, Anthony Perez")>]
+    [<assembly: AssemblyVersion("1.0")>]
+    [<assembly: AssemblyFileVersion("1.0")>]
+    ()
