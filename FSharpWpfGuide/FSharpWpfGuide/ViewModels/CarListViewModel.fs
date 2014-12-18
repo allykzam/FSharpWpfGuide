c7265eb8aba76964e3f18fb1fb79c88da87ab27d
Anthony Perez (Work) <Anthony.Perez@hsn.net>
2014-12-18 09:17:53 -0500

    Added CarListViewModel

---
+++ FSharpWpfGuide/FSharpWpfGuide/ViewModels/CarListViewModel.fs
@@ -0,0 +1,14 @@
+﻿namespace FSharpWpfGuide.ViewModels
+
+open FSharpWpfGuide
+
+type CarListViewModel() =
+    inherit FSharp.ViewModule.ViewModelBase()
+
+    let cars = System.Collections.ObjectModel.ObservableCollection<CarViewModel>()
+
+    do
+        cars.Add(new CarViewModel())
+        cars.Add(new CarViewModel({ Make = Models.Chevy; Model = "Camaro ZL1"; Year = 2015; Miles = 10; }))
+
+    member __.Cars with get () = cars
